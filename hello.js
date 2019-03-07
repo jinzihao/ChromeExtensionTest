98bbcc5e06c68cf16b73919c496283d96014b59b
jinzihao <903703287@qq.com>
2019-03-07 00:24:45 -0500

    Hello World, task 3

--- hello.js
+++ hello.js
@@ -6,5 +6,21 @@ function googleNewWindow() {
     chrome.windows.create({'url': 'https://google.com'});
 }
 
+function updateThumbupCounter() {
+    chrome.storage.sync.get(['thumbupCount'], function(result) {
+        document.getElementById("counter").innerHTML = result.thumbupCount;
+    });
+}
+
+function thumbup() {
+    chrome.storage.sync.get(['thumbupCount'], function(result) {
+        document.getElementById("counter").innerHTML = result.thumbupCount + 1;
+        chrome.storage.sync.set({'thumbupCount': result.thumbupCount + 1});
+    });
+}
+
 document.getElementById("googleButtonNewTab").onclick = googleNewTab;
 document.getElementById("googleButtonNewWindow").onclick = googleNewWindow;
+document.getElementById("thumbup").onclick = thumbup;
+
+updateThumbupCounter();
