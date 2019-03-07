178bd18297487267c3cdb9cdcc1f886763562da2
jinzihao <903703287@qq.com>
2019-03-07 00:47:29 -0500

    Hello World, task 4, update 1

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,4 @@
-document.body.innerHTML = "<h1>This is a webpage at " + window.location.href + "</h1>" + document.body.innerHTML;
+// According to https://piazza.com/class/jqo8q481kfe1u4?cid=156, what I need to do is simply call alert():
+alert("This is a webpage at " + window.location.href);
+// I can also modify the DOM, like this:
+// document.body.innerHTML = "<h1>This is a webpage at " + window.location.href + "</h1>" + document.body.innerHTML;
